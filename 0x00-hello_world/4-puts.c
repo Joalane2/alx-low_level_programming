d45b2ea6d730031f2817cfac552224712ff48289
Joalane2 <senatsijoalane@gmail.com>
2022-09-09 08:26:50 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - using puts 
- *
+ * main - using puts
  * return always zero
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
