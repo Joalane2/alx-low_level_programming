9c8cac45c56b179a43e604a899bf2643b773edb8
Joalane2 <senatsijoalane@gmail.com>
2022-09-11 07:26:12 -0700

    Size of a Variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,14 +11,12 @@ int main(void)
 	long int longintType;
 	long long int longlongintType;
 	float floatType;
-	double doubleType;
 	char charType;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(charType));
-	printf("Size of an int: %d byte(s)\n", sizeof(intType));
-	printf("size of a long int: %ld byte(s)\n", sizeof(longintType));
-	printf("size of a long long int: %d byte(s)\n", sizeof(longlongintType));
-	printf("size of a float: %d byte(s)\n", sizeof(floatType));
-
+	printf("Size of a char: %d byte(s)\n",(unsigned long)sizeof(charType));
+	printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(intType));
+	printf("size of a long int: %ld byte(s)\n", (unsigned long)sizeof(longintType));
+	printf("size of a long long int: %d byte(s)\n", (unsigned long)sizeof(longlongintType));
+	printf("size of a float: %d byte(s)\n", (unsigned long)sizeof(floatType));
 	return(0);
 }
