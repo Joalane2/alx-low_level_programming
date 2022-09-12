adf628a6b7fdc1fbffb689bd8be1ee83e66eb2d8
Joalane2 <senatsijoalane@gmail.com>
2022-09-12 09:43:07 -0700

    Size of Variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main(void)
 
 /**
- * Main - Entry point
- * Determining the sizes of the variable
- * Return 0 (success)
+ *main - Determining the sizes of the variables
+ *
+ *Return: Always 0 (success)
  */
+int main(void)
 {
 	int intType;
 	long int longintType;
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(charType));
 	printf("Size of an int: %d byte(s)\n", sizeof(intType));
 	printf("size of a long int: %ld byte(s)\n", sizeof(longintType));
-	printf("size of a long long int: %d byte(s)\n",sizeof(longlongintType));
+	printf("size of a long long int: %d byte(s)\n", sizeof(longlongintType));
 	printf("size of a float: %d byte(s)\n", sizeof(floatType));
-	return(0);
+	return (0);
 }
