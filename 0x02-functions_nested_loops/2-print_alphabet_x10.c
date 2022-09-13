75baad10e0de7d468a7653f35317eb30f3de0ab5
Joalane2 <senatsijoalane@gmail.com>
2022-09-13 13:56:11 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,14 @@ void print_alphabet_x10(void)
 {
 	int x=0;
 	while(x < 10)
-	{
-	  print_alphabet();
+	{    
+		char alpha;
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+		  _putchar(alpha);
+		}
+	  _putchar('\n');
 	  x++;
 	}
 }
